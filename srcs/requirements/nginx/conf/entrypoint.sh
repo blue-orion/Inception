ed41c36a31d00881149e49e29897969348978414
blue-orion <xogh080907@gmail.com>
2025-06-10 00:43:57 +0900

    mkcert

--- srcs/requirements/nginx/conf/entrypoint.sh
+++ srcs/requirements/nginx/conf/entrypoint.sh
@@ -12,6 +12,10 @@ openssl genrsa -out server.key 2048
 openssl req -new -key server.key -out server.csr	\
 	-subj "/C=KR/ST=Gyeonsan/L=Gyeonsan/O=42/OU=42/CN=localhost"
 openssl x509 -req -in server.csr -CA 42CA.crt -CAkey 42CA.key -CAcreateserial -out server.crt
+
+mkcert -install
+mkcert localhost
+
 cd /var/www/html
 
 nginx -g "daemon off;"
